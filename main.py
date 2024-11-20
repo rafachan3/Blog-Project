fdbb4a13d9567634f9bb1da49e0f0cbe9b34bff1
Rafael Chantres Garcia <81809918+rafachan3@users.noreply.github.com>
2024-11-20 13:10:03 -0600

    Initialize Flask app and fetch blog posts from API
    - Set up a basic Flask application in `main.py`.
    - Fetch blog post data from an external API using the `requests` library.
    - Create `Post` objects from the fetched data and store them in a list.
    - Render the `index.html` template with the list of `Post` objects.
    - Added `Post` class definition in `post.py`.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+import requests
+from post import Post
 
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
+print(post_objects)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_posts=post_objects)
 
 if __name__ == "__main__":
     app.run(debug=True)
