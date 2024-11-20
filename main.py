b40948d47702608729423ba68360dc12c75fa9e4
Rafael Chantres Garcia <81809918+rafachan3@users.noreply.github.com>
2024-11-20 14:13:00 -0600

    Add blog route & automate entry display
    - Changed the name of the function within the home route
    
    - Added a new route for the content of each individual blog post.
    
    -added logic in index.html that allows to create a div for each blog entry automatically

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ post_objects = []
 for post in posts:
     post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
     post_objects.append(post_obj)
-print(post_objects)
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def get_all_posts():
     return render_template("index.html", all_posts=post_objects)
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
